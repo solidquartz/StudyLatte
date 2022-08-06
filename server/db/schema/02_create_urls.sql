36526f15b8b9c4ef2e92c270a11ca577495a52ee
Sae <winny9641@gmail.com>
2022-08-06 13:50:46 -0400

    add users schema

--- server/db/schema/02_create_urls.sql
+++ server/db/schema/02_create_urls.sql
@@ -1,7 +1,7 @@
 -- schema/02_create_urls.sql
 DROP TABLE IF EXISTS urls CASCADE;
 -- CREATE URLS
-CREATE TABLE urls (
+CREATE TABLE study_rooms (
   id SERIAL PRIMARY KEY,
   user_id integer REFERENCES users(id) ON DELETE CASCADE NOT NULL,
   long_url character varying(255) NOT NULL,
