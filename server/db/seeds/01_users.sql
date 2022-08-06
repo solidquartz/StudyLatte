3701151e8e995c79dc9b32514a927bd78cc569e3
Sae <winny9641@gmail.com>
2022-08-06 14:09:47 -0400

    add seeds for users

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,8 +1,14 @@
 -- seeds/01_users.sql
 -- users seeds
-INSERT INTO users (id, email, password) VALUES (1, 'rick.sandchez@gmail.com', 'picklerick');
-INSERT INTO users (id, email, password) VALUES (2, 'lisa.simpson@gmail.com', 'ehhhhh');
-INSERT INTO users (id, email, password) VALUES (3, 'link@yahoo.com', 'hyrule');
-INSERT INTO users (id, email, password) VALUES (4, 'simon_bel123@mail.ca', 'dracula');
-INSERT INTO users (id, email, password) VALUES (5, 'all_might@academia.jp', 'plusUltra');
-INSERT INTO users (id, email, password) VALUES (6, 'mario@mushroomkindom.jp', 'plumber79');
+INSERT INTO users (name, display_name, email, password) VALUES ('James','CoolKid','james@gmail.com', 'password');
+INSERT INTO users (name, display_name, email, password) VALUES ('Sae','KingSae','liam@gmail.com', 'password');
+INSERT INTO users (name, display_name, email, password) VALUES ('Liam','Liamtheboy','liam@gmail.com', 'password');
+INSERT INTO users (name, display_name, email, password) VALUES ('Olivia','Olives','olivia@gmail.com', 'password');
+INSERT INTO users (name, display_name, email, password) VALUES ('Elijah','catlover','elijah@gmail.com', 'password');
+INSERT INTO users (name, display_name, email, password) VALUES ('Charlotte','snoops','charlotte@gmail.com', 'password');
+INSERT INTO users (name, display_name, email, password) VALUES ('Amelia','codingpotato','amelia@gmail.com', 'password');
+INSERT INTO users (name, display_name, email, password) VALUES ('Ava','AvaEva','ava@gmail.com', 'password');
+INSERT INTO users (name, display_name, email, password) VALUES ('Lucas','luckycharms','lucas@gmail.com', 'password');
+INSERT INTO users (name, display_name, email, password) VALUES ('Henry','hennessey','henry@gmail.com', 'password');
+INSERT INTO users (name, display_name, email, password) VALUES ('Evelyn','helloworld','evelyn@gmail.com', 'password');
+INSERT INTO users (name, display_name, email, password) VALUES ('Harper','HAPPYHarper','harper@gmail.com', 'password');
