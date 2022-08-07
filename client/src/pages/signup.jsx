e331445b3de93da22872c9f231ec3b11539fed62
Aky Mac Donald <akymacd@gmail.com>
2022-08-07 19:25:05 -0400

    Edit signup form to use chakra and yup with formik

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik, Field } from "formik";
+import { Formik, Form, useField } from "formik";
 import {
   Box,
   Button,
@@ -12,74 +12,97 @@ import {
   VStack,
   Heading
 } from "@chakra-ui/react";
+import * as Yup from 'yup';
 
-//for the form to work with ChakraUI, use <Input> and <Button> (capital letter)
+//using chakra with formik: https://chakra-ui.com/getting-started/with-formik
+//formik kitchen sink: https://codesandbox.io/s/formik-v2-tutorial-final-ge1pt?file=/src/index.js:263-759
+//yup validation: https://github.com/jquense/yup
 
-export const Signup = () => {
-
+const MyTextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
   return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
+//component:  
+export const Signup = ({ label, ...props }) => {
+  return (
+
     <main>
+      <Flex bg="gray.100" align="center" justify="center" h="100vh">
+        <Box bg="white" p={6} rounded="md" w={80}>
+          <Formik
+            initialValues={{
+              email: "",
+              username: "",
+              displayName: "",
+              password: "",
+              confirmPassword: ""
+            }}
 
-    <Flex bg="gray.100" align="center" justify="center" h="100vh">
-      <Box bg="white" p={6} rounded="md" w={80}>
-        <Formik
-          initialValues={{
-            email: "",
-            password: "",
-            rememberMe: false
-          }}
-          onSubmit={(values) => {
-            alert(JSON.stringify(values, null, 2));
-          }}
-        >
-          {({ handleSubmit, errors, touched }) => (
-              <form onSubmit={handleSubmit}>
-                
-                <VStack spacing={4} align="flex-start">
-                  <Heading>Create an Account</Heading>
-                <FormControl>
-                  <FormLabel htmlFor="email">Email Address</FormLabel>
-                  <Field
-                    as={Input}
-                    id="email"
-                    name="email"
-                    type="email"
-                    variant="filled"
-                  />
-                  </FormControl>
-                  
-                <FormControl isInvalid={!!errors.password && touched.password}>
-                  <FormLabel htmlFor="password">Password</FormLabel>
-                  <Field
-                    as={Input}
-                    id="password"
-                    name="password"
-                    type="password"
-                    variant="filled"
-                    validate={(value) => {
-                      let error;
+            //this will need updating
+            //add things such as lowercase & trim
+            //check yup docs
+            validationSchema={Yup.object({
+              email: Yup.string()
+                .email("Invalid email address")
+                .required("Required"),
+              username: Yup.string()
+                .max(15, "Must be 15 characters or less")
+                .required("Required"),
+              displayName: Yup.string()
+                .required("Required"),
+              password: Yup.string()
+                .required("Required"),
+              //add matches
+              confirmPassword: Yup.string()
+                .required("Required"),
+            })}
 
-                      if (value.length < 5) {
-                        error = "Password must contain at least 6 characters";
-                      }
 
-                      return error;
-                    }}
-                    />
-                    
-                  <FormErrorMessage>{errors.password}</FormErrorMessage>
-                  </FormControl>
-                  
-                <Button type="submit" colorScheme="purple" width="full">
-                  Sign Up
-                  </Button>
-                  
-              </VStack>
-            </form>
-          )}
-        </Formik>
-      </Box>
+
+            onSubmit={async (values, { setSubmitting }) => {
+              await new Promise(r => setTimeout(r, 500));
+              setSubmitting(false);
+            }}
+          >
+
+            <VStack spacing={4} align="flex-start">
+              <Heading>Create an Account</Heading>
+
+              <FormControl>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <MyTextInput
+                  name="email"
+                  type="text"
+                  placeholder="you@email.com"
+                />
+              </FormControl>
+
+              <FormControl>
+                <FormLabel htmlFor="username">Username</FormLabel>
+                <MyTextInput
+                  name="username"
+                  type="text"
+                  placeholder="username"
+                />
+              </FormControl>
+
+              <Button type="submit" colorScheme="purple" width="full">
+                Sign Up
+              </Button>
+
+            </VStack>
+          </Formik>
+        </Box>
       </Flex>
     </main>
   );
-                  };
+};
