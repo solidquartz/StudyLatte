b26e4e9373f2fb56b601ec79f3e956c03c53189d
Aky Mac Donald <akymacd@gmail.com>
2022-08-08 18:04:37 -0400

    Fixed DB typo; added initial values to Create Room form

--- client/src/pages/create-room.jsx
+++ client/src/pages/create-room.jsx
@@ -1,5 +1,15 @@
-// import { Link as RouterLink } from 'react-router-dom';
-import { Heading } from '@chakra-ui/react';
+import React from 'react';
+import { Formik } from "formik";
+import {
+  Box,
+  Button,
+  Flex,
+  VStack,
+  Heading,
+  ButtonGroup
+} from "@chakra-ui/react";
+import * as Yup from 'yup';
+import TextField from './textField';
 
 
 export const CreateRoom = () => {
@@ -10,6 +20,81 @@ export const CreateRoom = () => {
       <div>
         <Heading>Create a Study Room</Heading>
       </div>
+
+      <Formik
+        initialValues={{
+          title: "",
+          topic: "",
+          description: "",
+          max_capacity: null,
+          pomodoro: true,
+          study_time: 25,
+          break_time: 5,
+          long_break_time: 30,
+          reps: 2,
+          sound: true,
+          isPrivate: false,
+          password: "",
+          background_img: ""
+        }}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .required("Email required")
+            .email("Please use a valid email address"),
+          password: Yup.string()
+            .required("Password required")
+        })}
+        onSubmit={(values, actions) => {
+          alert(JSON.stringify(values, null, 2));
+          actions.resetForm();
+        }}
+      >
+
+        {formik => (
+          <Flex
+            bg="white.100"
+            align="center"
+          >
+            <Box
+              bg="white"
+              p={6}
+              rounded="md"
+              w={80}
+            >
+              <VStack
+                as="form"
+                mx="auto"
+                spacing={5}
+                justifyContent="center"
+                onSubmit={formik.handleSubmit}
+              >
+
+                <TextField
+                  name="email"
+                  type="email"
+                  placeholder="Email Address"
+                />
+
+                <TextField
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                />
+
+                <ButtonGroup gap="2">
+                  <Button type="button" colorScheme="gray" width="50%">
+                    Cancel
+                  </Button>
+                  <Button type="submit" colorScheme="teal" width="90%">
+                    Create Room
+                  </Button>
+                </ButtonGroup>
+
+              </VStack>
+            </Box>
+          </Flex>
+        )}
+      </Formik>
 
     </main>
   );
