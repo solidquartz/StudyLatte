61d4882f7d6f235129fab6acca8230397cf7c28c
Aky Mac Donald <akymacd@gmail.com>
2022-08-08 14:47:01 -0400

    Restyled header navbar

--- client/src/layout/header.jsx
+++ client/src/layout/header.jsx
@@ -1,14 +1,98 @@
 import { Link as RouterLink } from 'react-router-dom';
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink
+  Button,
+  Flex,
 } from '@chakra-ui/react';
 
 export const Header = () => {
   return (
     <nav>
-      <Breadcrumb>
+
+      <Flex
+        pos="fixed"
+        top="1rem"
+        right="1rem"
+        align="center"
+      >
+        <Flex>
+          
+          <RouterLink to="/home">
+            <Button
+              as="button"
+              variant="ghost"
+              aria-label="home"
+              my={5}
+              w="100%"
+            >
+              Home
+            </Button>
+          </RouterLink>
+
+          <RouterLink to="/join-room">
+            <Button
+              as="button"
+              variant="ghost"
+              aria-label="join-room"
+              my={5}
+              w="100%"
+            >
+              Join Room
+            </Button>
+          </RouterLink>
+
+          <RouterLink to="/create-room">
+            <Button
+              as="button"
+              variant="ghost"
+              aria-label="create-room"
+              my={5}
+              w="100%"
+            >
+              Create Room
+            </Button>
+          </RouterLink>
+
+          <RouterLink to="/login">
+            <Button
+              as="button"
+              variant="ghost"
+              aria-label="login"
+              my={5}
+              w="100%"
+            >
+              Log In
+            </Button>
+          </RouterLink>
+
+          <RouterLink to="/signup">
+            <Button
+              as="button"
+              variant="ghost"
+              aria-label="signup"
+              my={5}
+              w="100%"
+            >
+              Sign Up
+            </Button>
+          </RouterLink>
+
+          <RouterLink to="/home">
+            <Button
+              as="button"
+              variant="ghost"
+              aria-label="logout"
+              my={5}
+              w="100%"
+            >
+              Log Out
+            </Button>
+          </RouterLink>
+
+
+        </Flex>
+      </Flex>
+
+      {/* <Breadcrumb>
         <BreadcrumbItem>
           <BreadcrumbLink as={RouterLink} to="/">Home</BreadcrumbLink>
         </BreadcrumbItem>
@@ -30,8 +114,8 @@ export const Header = () => {
         <BreadcrumbItem>
           <BreadcrumbLink as={RouterLink} to="/test">DB Test</BreadcrumbLink>
         </BreadcrumbItem>
+      </Breadcrumb> */}
 
-      </Breadcrumb>
     </nav>
   );
 };
