1b86b359aedb4164107858100810e471453a7fc6
Aky Mac Donald <akymacd@gmail.com>
2022-08-07 20:53:27 -0400

    Refactor form, added validation

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik, Form, useField } from "formik";
+import { Formik, useFormik } from "formik";
 import {
   Box,
   Button,
@@ -15,94 +15,133 @@ import {
 import * as Yup from 'yup';
 
 //using chakra with formik: https://chakra-ui.com/getting-started/with-formik
-//formik kitchen sink: https://codesandbox.io/s/formik-v2-tutorial-final-ge1pt?file=/src/index.js:263-759
+//formik docs: https://formik.org/docs/tutorial
 //yup validation: https://github.com/jquense/yup
 
-const MyTextInput = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
-  return (
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
-
 //component:  
-export const Signup = ({ label, ...props }) => {
+export const Signup = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      username: "",
+      displayName: "",
+      password: "",
+      confirmPassword: ""
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .required("Email required")
+        .email("Please use a valid email address"),
+      username: Yup.string()
+        .required("Username required")
+        .max(10, "Username is too long (max. 15)")
+        .min(4, "Username is too short (min. 4"),
+      displayName: Yup.string()
+        .required("Display Name required")
+        .max(10, "Display Name is too long (max 15)"),
+      password: Yup.string()
+        .required("Password required")
+        .min(10, "Password is too short (min. 10)"),
+      confirmPassword: Yup.string()
+        .required("Please type your password again")
+        .min(10, "Password is too short (min. 10)")
+    }),
+    onSubmit: (values, actions) => {
+      alert(JSON.stringify(values, null, 5));
+      actions.resetForm();
+    },
+  });
+
   return (
-
     <main>
-      <Flex bg="gray.100" align="center" justify="center" h="100vh">
-        <Box bg="white" p={6} rounded="md" w={80}>
-          <Formik
-            initialValues={{
-              email: "",
-              username: "",
-              displayName: "",
-              password: "",
-              confirmPassword: ""
-            }}
 
-            //this will need updating
-            //add things such as lowercase & trim
-            //check yup docs
-            validationSchema={Yup.object({
-              email: Yup.string()
-                .email("Invalid email address")
-                .required("Required"),
-              username: Yup.string()
-                .max(15, "Must be 15 characters or less")
-                .required("Required"),
-              displayName: Yup.string()
-                .required("Required"),
-              password: Yup.string()
-                .required("Required"),
-              //add matches
-              confirmPassword: Yup.string()
-                .required("Required"),
-            })}
-
-
-
-            onSubmit={async (values, { setSubmitting }) => {
-              await new Promise(r => setTimeout(r, 500));
-              setSubmitting(false);
-            }}
+      <Flex
+        bg="gray.100"
+        align="center"
+        justify="center"
+        h="100vh"
+      >
+        <Box
+          bg="white"
+          p={6}
+          rounded="md"
+          w={80}
+        >
+          <VStack
+            as="form"
+            mx="auto"
+            spacing={4}
+            justifyContent="center"
+            onSubmit={formik.handleSubmit}
           >
+            <Heading>Sign Up</Heading>
 
-            <VStack spacing={4} align="flex-start">
-              <Heading>Create an Account</Heading>
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input
+                name="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                type="text"
+                placeholder="you@email.com"
+              />
+              <FormErrorMessage>(formik.errors.email)</FormErrorMessage>
+            </FormControl>
 
-              <FormControl>
-                <FormLabel htmlFor="email">Email</FormLabel>
-                <MyTextInput
-                  name="email"
-                  type="text"
-                  placeholder="you@email.com"
-                />
-              </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input
+                name="username"
+                onChange={formik.handleChange}
+                value={formik.values.username}
+                type="text"
+              />
+              <FormErrorMessage>(formik.errors.username)</FormErrorMessage>
+            </FormControl>
 
-              <FormControl>
-                <FormLabel htmlFor="username">Username</FormLabel>
-                <MyTextInput
-                  name="username"
-                  type="text"
-                  placeholder="username"
-                />
-              </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="username">Display Name</FormLabel>
+              <Input
+                name="displayName"
+                onChange={formik.handleChange}
+                value={formik.values.displayName}
+                type="text"
+              />
+              <FormErrorMessage>(formik.errors.displayName)</FormErrorMessage>
+            </FormControl>
 
-              <Button type="submit" colorScheme="purple" width="full">
-                Sign Up
-              </Button>
+            <FormControl>
+              <FormLabel htmlFor="username">Password</FormLabel>
+              <Input
+                name="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                type="text"
+              />
+              <FormErrorMessage>(formik.errors.password)</FormErrorMessage>
+            </FormControl>
 
-            </VStack>
-          </Formik>
+            <FormControl>
+              <FormLabel htmlFor="username">Confirm Password</FormLabel>
+              <Input
+                name="confirmPassword"
+                onChange={formik.handleChange}
+                value={formik.values.confirmPassword}
+                type="text"
+              />
+              <FormErrorMessage>(formik.errors.confirmPassword)</FormErrorMessage>
+            </FormControl>
+
+            <Button type="submit" colorScheme="purple" width="full">
+              Sign Up
+            </Button>
+
+          </VStack>
+
         </Box>
       </Flex>
+
     </main>
   );
 };
