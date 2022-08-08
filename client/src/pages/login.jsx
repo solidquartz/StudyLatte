3ec5c6b6aac7462c6a31773c6951847a11ffe23b
Aky Mac Donald <akymacd@gmail.com>
2022-08-08 16:01:41 -0400

    Added basic styling for clarity

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -18,68 +18,69 @@ import TextField from './textField';
 export const Login = () => {
 
   return (
+    <main>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .required("Email required")
+            .email("Please use a valid email address"),
+          password: Yup.string()
+            .required("Password required")
+        })}
+        onSubmit={(values, actions) => {
+          alert(JSON.stringify(values, null, 2));
+          actions.resetForm();
+        }}
+      >
 
-    <Formik
-      initialValues={{
-        email: "",
-        password: "",
-      }}
-      validationSchema={Yup.object({
-        email: Yup.string()
-          .required("Email required")
-          .email("Please use a valid email address"),
-        password: Yup.string()
-          .required("Password required")
-      })}
-      onSubmit={(values, actions) => {
-        alert(JSON.stringify(values, null, 2));
-        actions.resetForm();
-      }}
-    >
-
-      {formik => (
-        <Flex
-          bg="gray.100"
-          align="center"
-          justify="center"
-          h="100vh"
-        >
-          <Box
-            bg="white"
-            p={6}
-            rounded="md"
-            w={80}
+        {formik => (
+          <Flex
+            bg="white.100"
+            align="center"
+            justify="center"
+            h="100vh"
           >
-            <VStack
-              as="form"
-              mx="auto"
-              spacing={5}
-              justifyContent="center"
-              onSubmit={formik.handleSubmit}
+            <Box
+              bg="white"
+              p={6}
+              rounded="md"
+              w={80}
             >
-              <Heading>Log In</Heading>
+              <VStack
+                as="form"
+                mx="auto"
+                spacing={5}
+                justifyContent="center"
+                onSubmit={formik.handleSubmit}
+              >
+                <Heading>Log In</Heading>
 
-              <TextField
-                name="email"
-                type="email"
-                placeholder="Email Address"
-              />
+                <TextField
+                  name="email"
+                  type="email"
+                  placeholder="Email Address"
+                />
 
-              <TextField
-                name="password"
-                type="password"
-                placeholder="Password"
-              />
+                <TextField
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                />
 
-              <Button type="submit" colorScheme="purple" width="full">
-                Log In
-              </Button>
+                <Button type="submit" colorScheme="purple" width="full">
+                  Log In
+                </Button>
 
-            </VStack>
-          </Box>
-        </Flex>
-      )}
-    </Formik>
+              </VStack>
+            </Box>
+          </Flex>
+        )}
+      </Formik>
 
+    </main>
   );
 };
