852620e98fe2a332f37a4e23e35abe0c69553b8d
Sae <winny9641@gmail.com>
2022-08-09 16:26:48 -0400

    add study_room seed

--- server/db/seeds/02_study_rooms.sql
+++ server/db/seeds/02_study_rooms.sql
@@ -1,4 +1,4 @@
 -- seeds/02_urls.sql
 -- Seed Rick
-INSERT INTO study_rooms  (title, topic, sound, max_capacity, isPriavte, pomodoro)
- VALUES ('Example study room','react',false, 8,false,true);
+INSERT INTO study_rooms  (title, topic, sound, max_capacity, isPriavte, pomodoro,entered_users)
+ VALUES ('Example study room','react',false, 8,false,true, ARRAY[1,2,3]);
