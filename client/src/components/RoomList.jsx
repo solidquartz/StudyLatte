7172e5441ee6c328889f317f4f9850b169dd4906
ricardomoura1979 <73795249+ricardomoura1979@users.noreply.github.com>
2022-08-12 12:48:16 -0500

    added useEffect and and RoomListItem key

--- client/src/components/RoomList.jsx
+++ client/src/components/RoomList.jsx
@@ -1,46 +1,36 @@
 import {
   Box
 } from '@chakra-ui/react';
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import {RoomListItem} from './RoomListItem';
 
 //will need to import this into RoomList/join-room wherever we use it
 
 export const RoomList = (props) => {
+  const [roomList, setRoomList] = useState([]);
+  
+  useEffect(() => {
+    axios.get("/study_rooms").then((response) => {
+      setRoomList(response.data);
+      
+    });
+
+
+
+},[])
+
+
+
+
+
+
+
+
 return (
+
   <ul>
-  {props.rooms.map(room => <Box maxW='md' borderWidth='2px' borderRadius='lg' overflow='hidden'>
-      <Box p='6'>
-        
-        <Box display='flex' alignItems='baseline'> {room.title}
-        </Box>
-
-        <Box
-          color='gray.500'
-          fontWeight='semibold'
-          letterSpacing='wide'
-          fontSize='xs'
-          textTransform='uppercase'
-          ml='2'
-        >
-          {room.name}
-        </Box>
-
-        <Box
-          mt='1'
-          fontWeight='semibold'
-          as='h4'
-          lineHeight='tight'
-          noOfLines={3}
-        >
-          {room.description}
-        </Box>
-
-        <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-          {room.users}
-        </Box>
-
-      </Box>
-    </Box>)}
+  {roomList.map(studyroom=> <RoomListItem key= {studyroom.id} {...studyroom}/>)}
 
 </ul>
 
