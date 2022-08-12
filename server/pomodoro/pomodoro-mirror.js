95125823f1d2f37908db03d04d39dca59f0e443a
ricardomoura1979 <73795249+ricardomoura1979@users.noreply.github.com>
2022-08-12 08:01:22 -0500

    poromodo mirror in the server side to reflect to client side

---
+++ server/pomodoro/pomodoro-mirror.js
@@ -0,0 +1,12 @@
+// const time = 25;
+
+// socket.on("start-timer", function() {
+//     var timer = setInterval(function() {
+//         time--;
+//         if (time <= 0) {
+//             clearInterval(timer);
+//             return;
+//         }
+//         socket.emit("timer", time);
+//     }, 1000);
+// });
