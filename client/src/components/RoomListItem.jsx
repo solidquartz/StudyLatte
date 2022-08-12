94dcab4fc7e0efae173f8172ae8d20a739bc3bec
ricardomoura1979 <73795249+ricardomoura1979@users.noreply.github.com>
2022-08-12 18:27:20 -0500

    added Join button in the chat list

--- client/src/components/RoomListItem.jsx
+++ client/src/components/RoomListItem.jsx
@@ -1,46 +1,66 @@
 import {
-  Box
+  Box,
+  Button
 } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+
+
+
+
 export const RoomListItem = (props) => {
-  
+ 
 
-  return (
-  <Box maxW='md' borderWidth='2px' borderRadius='lg' overflow='hidden'>
-      <Box p='6'>
-        
-        <Box display='flex' alignItems='baseline'> {props.title}
+    return (
+      <Box maxW='md' borderWidth='2px' borderRadius='lg' overflow='hidden'>
+        <Box p='6'>
+
+          <Box display='flex' alignItems='baseline'> {props.title}
+          </Box>
+
+          <Box
+            color='gray.500'
+            fontWeight='semibold'
+            letterSpacing='wide'
+            fontSize='xs'
+            textTransform='uppercase'
+            ml='2'
+          >
+            {props.topic}
+          </Box>
+
+          <Box
+            mt='1'
+            fontWeight='semibold'
+            as='h4'
+            lineHeight='tight'
+            noOfLines={3}
+          >
+            {props.description}
+          </Box>
+
+          <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+            {props.entered_users.length}
+
+          
+            <Button
+              as="button"
+              variant="ghost"
+              aria-label="join-room"
+              my={5}
+              w="100%"
+            >
+              Join Room
+            </Button>
+
+          </Box>
+
         </Box>
-
-        <Box
-          color='gray.500'
-          fontWeight='semibold'
-          letterSpacing='wide'
-          fontSize='xs'
-          textTransform='uppercase'
-          ml='2'
-        >
-          {props.topic}
-        </Box>
-
-        <Box
-          mt='1'
-          fontWeight='semibold'
-          as='h4'
-          lineHeight='tight'
-          noOfLines={3}
-        >
-          {props.description}
-        </Box>
-
-        <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-          {props.entered_users.length}
-        </Box>
-
       </Box>
-    </Box>
 
 
 
-  )
 
+
+    )
+  
 }
