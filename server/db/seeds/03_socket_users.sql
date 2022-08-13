cfc5b70bd6237a246e979776342e015ba3696dd7
Sae <winny9641@gmail.com>
2022-08-13 00:02:08 -0400

    add seeds for socket_users

---
+++ server/db/seeds/03_socket_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO socket_users (socket_id,username) VALUES ('James','aaaa');
+INSERT INTO socket_users (socket_id,username) VALUES ('Lauren','bbbb');
+INSERT INTO socket_users (socket_id,username) VALUES ('lulu','cccc');
---
+++ server/routes/socket_users.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+module.exports = (db) => {
+  // all routes will go here 
+  router.get('/', (req, res) => {
+    const command = "SELECT * FROM socket_users";
+    db.query(command).then(data => {
+      res.json(data.rows);
+    });
+  });
+
+
+
+
+
+
+  return router;
+};
