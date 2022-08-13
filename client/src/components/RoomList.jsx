0603e5cbdaeb20463c6c07bed7c0b36b4c5f37d9
ricardomoura1979 <73795249+ricardomoura1979@users.noreply.github.com>
2022-08-13 18:26:26 -0500

    Update RoomList.jsx

--- client/src/components/RoomList.jsx
+++ client/src/components/RoomList.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import React, {useEffect, useState} from 'react';
 import {RoomListItem} from './RoomListItem';
 
+
 //will need to import this into RoomList/join-room wherever we use it
 
 export const RoomList = (props) => {
