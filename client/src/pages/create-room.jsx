adface8d0a3034a8a5b54287e315dd3fc5f20cf4
Aky Mac Donald <akymacd@gmail.com>
2022-08-14 14:17:26 -0400

    Fixed create room style

--- client/src/pages/create-room.jsx
+++ client/src/pages/create-room.jsx
@@ -26,192 +26,100 @@ export const CreateRoom = () => {
   return (
     <main>
 
+      <div className='create-room-container'>
+        <div className='create-room-wrapper'>
+          <div className='create-form'>
+            <h3>Create a Study Room</h3>
+            <Formik
+              initialValues={{
+                title: "",
+                topic: "",
+                description: "",
+              }}
+              validationSchema={Yup.object({
+                title: Yup.string()
+                  .required("Please give your room a title"),
+                topic: Yup.string()
+                  .required("Please write a descriptive topic"),
+                description: Yup.string()
+                  .required("Please write a description"),
+              })}
+              onSubmit={(values, actions) => {
 
-      <Formik
-        initialValues={{
-          title: "",
-          topic: "",
-          description: "",
-        }}
-        validationSchema={Yup.object({
-          title: Yup.string()
-            .required("Please give your room a title"),
-          topic: Yup.string()
-            .required("Please write a descriptive topic"),
-          description: Yup.string()
-            .required("Please write a description"),
-        })}
-        onSubmit={(values, actions) => {
+                axios.post("/study_rooms/add_new_room", {
+                  title: values.title,
+                  topic: values.topic,
+                  description: values.description,
+                }).then((response) => {
+                  console.log(response.data.id);
+                  navigate(`/join-room?roomId=${response.data.id}`);
 
-          axios.post("/study_rooms/add_new_room", {
-              title: values.title,
-              topic: values.topic,
-              description: values.description,
-            }).then((response) => {
-              console.log(response.data.id)
-              navigate(`/join-room?roomId=${response.data.id}`)
-           
-            });
-          // alert(JSON.stringify(values, null, 3));
-          actions.resetForm();
-        }}
-      >
-
-        {formik => (
-          <Flex
-            bg="white.100"
-            align="center"
-          >
-            <Box
-              bg="white"
-              p={6}
-              rounded="md"
-              w={80}
+                });
+                // alert(JSON.stringify(values, null, 3));
+                actions.resetForm();
+              }}
             >
-              <VStack
-                as="form"
-                mx="auto"
-                spacing={5}
-                justifyContent="center"
-                onSubmit={formik.handleSubmit}
-              >
 
-                <TextField
-                  name="title"
-                  type="text"
-                  placeholder="Room Title"
-                />
-
-                <TextField
-                  name="topic"
-                  type="text"
-                  placeholder="Study Topic"
-                />
-
-                <TextAreaField
-                  name="description"
-                  type="text"
-                  placeholder='Description' />
-
-
-
-                <ButtonGroup gap="2">
-                  <Button type="button" colorScheme="gray" width="50%">
-                    <RouterLink to="/join-room">
-                      Cancel
-                    </RouterLink>
-                 
-                  </Button>
-
-                  
-                  <Button type="submit" colorScheme="teal" width="90%">
-                    Create Room
-                  </Button>
-                  
-                </ButtonGroup>
-
-              </VStack>
-            </Box>
-          </Flex>
-        )}
-      </Formik>
-=======
-      <div className="create-room-container">
-        <div className="joinChatWrapper">
-          <form className="form">
-            <div className="input-group">
-
-              <h3>Create a Study Room</h3>
-
-              <Formik
-                initialValues={{
-                  title: "",
-                  topic: "",
-                  description: "",
-                }}
-                validationSchema={Yup.object({
-                  title: Yup.string()
-                    .required("Please give your room a title"),
-                  topic: Yup.string()
-                    .required("Please write a descriptive topic"),
-                  description: Yup.string()
-                    .required("Please write a description"),
-                })}
-                onSubmit={(values, actions) => {
-
-                  axios.post("/study_rooms/add_new_room", {
-                    title: values.title,
-                    topic: values.topic,
-                    description: values.description,
-                  }).then((response) => {
-                    console.log(response.data);
-
-                  });
-                  // alert(JSON.stringify(values, null, 3));
-                  actions.resetForm();
-                }}
-              >
-
-                {formik => (
-                  <Flex
-                    bg="white.100"
-                    align="center"
+              {formik => (
+                <Flex
+                  bg="black.100"
+                  align="center"
+                >
+                  <Box
+                    bg="black.100"
+                    p={6}
+                    rounded="md"
+                    w={80}
                   >
-                    <Box
-                      bg="black.100"
-                      p={6}
-                      rounded="md"
-                      w={80}
+                    <VStack
+                      as="form"
+                      mx="auto"
+                      spacing={5}
+                      justifyContent="center"
+                      onSubmit={formik.handleSubmit}
                     >
-                      <VStack
-                        as="form"
-                        mx="auto"
-                        spacing={5}
-                        justifyContent="center"
-                        onSubmit={formik.handleSubmit}
-                      >
 
-                        <TextField
-                          name="title"
-                          type="text"
-                          placeholder="Room Title"
-                        />
+                      <TextField
+                        name="title"
+                        type="text"
+                        placeholder="Room Title"
+                      />
 
-                        <TextField
-                          name="topic"
-                          type="text"
-                          placeholder="Study Topic"
-                        />
+                      <TextField
+                        name="topic"
+                        type="text"
+                        placeholder="Study Topic"
+                      />
 
-                        <TextAreaField
-                          name="description"
-                          type="text"
-                          placeholder='Description' />
+                      <TextAreaField
+                        name="description"
+                        type="text"
+                        placeholder='Description' />
 
 
 
-                        <ButtonGroup gap="2">
-                          <Button type="button" colorScheme="whiteAlpha" width="50%">
-                            <RouterLink to="/join-room">
-                              Cancel
-                            </RouterLink>
-                          </Button>
+                      <ButtonGroup gap="2">
+                        <Button type="button" colorScheme="whiteAlpha" width="50%">
+                          <RouterLink to="/join-room">
+                            Cancel
+                          </RouterLink>
 
-                          <Button type="submit" colorScheme="cyan" width="90%">
-                            Create Room
-                          </Button>
-                        </ButtonGroup>
+                        </Button>
 
-                      </VStack>
-                    </Box>
-                  </Flex>
-                )}
-              </Formik>
-            </div>
-          </form>
+
+                        <Button type="submit" colorScheme="cyan" width="90%">
+                          Create Room
+                        </Button>
+
+                      </ButtonGroup>
+
+                    </VStack>
+                  </Box>
+                </Flex>
+              )}
+            </Formik>
+          </div>
         </div>
-
-
       </div>
     </main>
   );
