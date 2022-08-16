94e642ac29e16ad37a1ac0e1a19dc864c5c845e9
ricardomoura1979 <73795249+ricardomoura1979@users.noreply.github.com>
2022-08-16 10:13:49 -0500

    added a functionality to random pic on avatars inside the room

--- client/src/rooms/users.jsx
+++ client/src/rooms/users.jsx
@@ -2,24 +2,25 @@ import {
   Heading,
   Avatar,
   Stack,
-  Text
+  Text,
+  propNames
 } from "@chakra-ui/react";
 
+ const avatarImages=['https://thumbs.dreamstime.com/b/coffee-cup-roast-grains-detailed-illustration-61268266.jpg', 'https://thumbs.dreamstime.com/b/coffee-cup-14555729.jpg', 'https://thumbs.dreamstime.com/b/coffee-sign-14306782.jpg', 'https://thumbs.dreamstime.com/b/coffee-cup-ilustration-39491193.jpg', 'https://thumbs.dreamstime.com/b/coffee-cup-19621825.jpg', 'https://thumbs.dreamstime.com/b/coffee-685132.jpg', 'https://thumbs.dreamstime.com/b/coffee-cup-vector-icon-steam-hot-drink-81730552.jpg', 'https://thumbs.dreamstime.com/b/coffee-cup-icon-isolated-flat-symbol-vector-sign-illustration-white-coffee-cup-icon-isolated-flat-symbol-vector-sign-107298476.jpg'];
+ console.log(avatarImages)
+
 export const Users = (props) => {
-  // const users = usersList.map(user => {
-  //   return (<Users username = {user}/>)
-  // })
-
-
+  
+  let randomAvatar = Math.floor(Math.random() * avatarImages.length);
+  let picture = avatarImages[randomAvatar]
 
   return (
     <>
        
       <Stack direction='column'>
-        {/* <Avatar name='Oshigaki Kisame' src='https://bit.ly/broken-link' />
         
-        <Avatar name='Sasuke Uchiha' src='https://bit.ly/broken-link' /> */}
-        <Avatar src='https://bit.ly/broken-link' />
+        <Avatar src={picture} />,
+       
       </Stack>
       <Text>{props.username}</Text>
     </>
